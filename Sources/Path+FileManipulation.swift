020e60cf4c0b65da69a152d64ec4d04325a745fd
shnhrrsn <shnhrrsn@gmail.com>
2016-04-17 11:15:20 -0400

    Path+ReadWrite.swift is foundation free

--- Sources/Path+FileManipulation.swift
+++ Sources/Path+FileManipulation.swift
@@ -1,16 +1,27 @@
+import OperatingSystem
+
 extension Path {
+
+    enum FileError: ErrorProtocol {
+        case unimplemented
+        case mkdir(Int)
+        case move(Int)
+        case delete(Int)
+        case symlink(Int)
+    }
+
     /// Create the directory.
     ///
     /// - Note: This method fails if any of the intermediate parent directories does not exist.
     ///   This method also fails if any of the intermediate path elements corresponds to a file and
     ///   not a directory.
     ///
-    public func mkdir() throws -> () {
-        #if os(Linux)
-            try Path.fileManager.createDirectoryAtPath(self.path, withIntermediateDirectories: false, attributes: nil)
-        #else
-            try Path.fileManager.createDirectory(atPath: self.path, withIntermediateDirectories: false, attributes: nil)
-        #endif
+    public func mkdir() throws {
+        let result = OperatingSystem.mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO)
+
+        if result != 0 && result != EEXIST {
+            throw FileError.mkdir(Int(errno))
+        }
     }
 
     /// Create the directory and any intermediate parent directories that do not exist.
@@ -18,12 +29,22 @@ extension Path {
     /// - Note: This method fails if any of the intermediate path elements corresponds to a file and
     ///   not a directory.
     ///
-    public func mkpath() throws -> () {
-        #if os(Linux)
-            try Path.fileManager.createDirectoryAtPath(self.path, withIntermediateDirectories: true, attributes: nil)
-        #else
-            try Path.fileManager.createDirectory(atPath: self.path, withIntermediateDirectories: true, attributes: nil)
-        #endif
+    public func mkpath() throws {
+        var path = ""
+
+        for component in self.components {
+            path += component
+
+            guard component != "/" else {
+                continue
+            }
+
+            let result = OperatingSystem.mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO)
+
+            if result != 0 && result != EEXIST {
+                throw FileError.mkdir(Int(errno))
+            }
+        }
     }
 
     /// Delete the file or directory.
@@ -31,12 +52,20 @@ extension Path {
     /// - Note: If the path specifies a directory, the contents of that directory are recursively
     ///   removed.
     ///
-    public func delete() throws -> () {
-        #if os(Linux)
-            try Path.fileManager.removeItemAtPath(self.path)
-        #else
-            try Path.fileManager.removeItem(atPath: self.path)
-        #endif
+    public func delete() throws {
+        let result = nftw(self.path, { (path, sb, typeflag, ftw) -> Int32 in
+            let result = remove(path)
+
+            if result != 0 {
+                perror(path)
+            }
+
+            return result
+        }, 64, FTW_DEPTH | FTW_PHYS)
+
+        if result != 0 {
+            throw FileError.delete(Int(errno))
+        }
     }
 
     /// Move the file or directory to a new location synchronously.
@@ -44,12 +73,12 @@ extension Path {
     /// - Parameter destination: The new path. This path must include the name of the file or
     ///   directory in its new location.
     ///
-    public func move(destination: Path) throws -> () {
-        #if os(Linux)
-            try Path.fileManager.moveItemAtPath(self.path, toPath: destination.path)
-        #else
-            try Path.fileManager.moveItem(atPath: self.path, toPath: destination.path)
-        #endif
+    public func move(destination: Path) throws {
+        let result = rename(path, destination.path)
+
+        if result != 0 {
+            throw FileError.move(Int(errno))
+        }
     }
 
     /// Copy the file or directory to a new location synchronously.
@@ -57,36 +86,30 @@ extension Path {
     /// - Parameter destination: The new path. This path must include the name of the file or
     ///   directory in its new location.
     ///
-    public func copy(destination: Path) throws -> () {
-        #if os(Linux)
-            try Path.fileManager.copyItemAtPath(self.path, toPath: destination.path)
-        #else
-            try Path.fileManager.copyItem(atPath: self.path, toPath: destination.path)
-        #endif
+    public func copy(destination: Path) throws {
+        // TODO
+        throw FileError.unimplemented
     }
 
     /// Creates a hard link at a new destination.
     ///
     /// - Parameter destination: The location where the link will be created.
     ///
-    public func link(destination: Path) throws -> () {
-        #if os(Linux)
-            try Path.fileManager.linkItemAtPath(self.path, toPath: destination.path)
-        #else
-            try Path.fileManager.linkItem(atPath: self.path, toPath: destination.path)
-        #endif
+    public func link(destination: Path) throws {
+        // TODO
+        throw FileError.unimplemented
     }
 
     /// Creates a symbolic link at a new destination.
     ///
     /// - Parameter destintation: The location where the link will be created.
     ///
-    public func symlink(destination: Path) throws -> () {
-        #if os(Linux)
-            try Path.fileManager.createSymbolicLinkAtPath(self.path, withDestinationPath: destination.path)
-        #else
-            try Path.fileManager.createSymbolicLink(atPath: self.path, withDestinationPath: destination.path)
-        #endif
+    public func symlink(destination: Path) throws {
+        let result = OperatingSystem.symlink(destination.path, self.path)
+
+        if result != 0 {
+            throw FileError.symlink(Int(errno))
+        }
     }
 
 }
