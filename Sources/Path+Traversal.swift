d0f4b4f139b62d501278badee1346dc9dc268578
shnhrrsn <shnhrrsn@gmail.com>
2016-04-17 12:08:10 -0400

    Path+Traversal.swift is foundation free

--- Sources/Path+Traversal.swift
+++ Sources/Path+Traversal.swift
@@ -1,10 +1,17 @@
+import OperatingSystem
+
 extension Path {
+
+    enum TraversalError: ErrorProtocol {
+        case opendir(Int)
+    }
+
     /// Get the parent directory
     ///
     /// - Returns: the normalized path of the parent directory
     ///
     public func parent() -> Path {
-        return self + ".."
+        return self + "/.."
     }
 
     /// Performs a shallow enumeration in a directory
@@ -12,15 +19,23 @@ extension Path {
     /// - Returns: paths to all files, directories and symbolic links contained in the directory
     ///
     public func children() throws -> [Path] {
-        #if os(Linux)
-            return try Path.fileManager.contentsOfDirectoryAtPath(path).map {
-                self + Path($0)
-            }
-        #else
-            return try Path.fileManager.contentsOfDirectory(atPath:path).map {
-                self + Path($0)
-            }
-        #endif
+        let dir = opendir(path)
+
+        if dir == nil {
+            throw TraversalError.opendir(Int(errno))
+        }
+
+        defer {
+            closedir(dir)
+        }
+
+        var children = [Path]()
+
+        try read(dir) { (name, _) in
+            children.append(self + Path(name))
+        }
+
+        return children
     }
 
     /// Performs a deep enumeration in a directory
@@ -29,15 +44,50 @@ extension Path {
     ///   any subdirectory.
     ///
     public func recursiveChildren() throws -> [Path] {
-        #if os(Linux)
-            return try Path.fileManager.subpathsOfDirectoryAtPath(path).map {
-                self + Path($0)
+        let dir = opendir(path)
+
+        if dir == nil {
+            throw TraversalError.opendir(Int(errno))
+        }
+
+        defer {
+            closedir(dir)
+        }
+
+        var children = [Path]()
+
+        try read(dir) { (name, type) in
+            let path = self + Path(name)
+            children.append(path)
+
+            if type == DT_DIR {
+                children += try path.recursiveChildren()
             }
-        #else
-            return try Path.fileManager.subpathsOfDirectory(atPath:path).map {
-                self + Path($0)
-            }
-        #endif
+        }
+
+        return children
     }
 
+}
+
+private func read(dir: UnsafeMutablePointer<DIR>, handler: (name: String, type: Int32?) throws -> Void) throws {
+    var entry = readdir(dir)
+
+    while entry != nil {
+        let name = withUnsafePointer(&entry.pointee.d_name, { (ptr) -> String? in
+            let int8Ptr = unsafeBitCast(ptr, to: UnsafePointer<Int8>.self)
+            return String(cString: int8Ptr)
+        })
+
+        if let name = name where name != "." && name != ".." {
+            let type = withUnsafePointer(&entry.pointee.d_type, { (ptr) -> Int32? in
+                let int32Ptr = unsafeBitCast(ptr, to: UnsafePointer<UInt8>.self)
+                return Int32(int32Ptr.pointee)
+            })
+
+            try handler(name: name, type: type)
+        }
+
+        entry = readdir(dir)
+    }
 }
