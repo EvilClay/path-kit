df006ae335bb238ab4f5e4771078f69a1946893f
shnhrrsn <shnhrrsn@gmail.com>
2016-04-17 15:06:31 -0400

    Actually seeded random in UUID so random numbers aren’t incremental

--- Sources/UUID.swift
+++ Sources/UUID.swift
@@ -1,12 +1,16 @@
 import OperatingSystem
 
 final internal class UUID {
+    private static var ordinal = UInt32(0)
+    private static var lastRandom = UInt32(0)
 
     // NOTE: uuid_t doesn’t exist in glibc
     // This is prone to collisions and should be replaced
     static func make() -> String {
         // Format: XXXXXXXX-XXXX-4XXX-YXXX-XXXXXXXXXXXX
         var uuid = ""
+
+        let base = UInt32(time(nil)) + UInt32(getpid())
 
         for x in 0..<31 {
             if x == 8 || x == 15 || x == 19 {
@@ -15,7 +19,11 @@ final internal class UUID {
                 uuid += "-4"
             }
 
+            ordinal += 1
+            srandom(base + ordinal + lastRandom)
+
             let r = Int((random() % 16) | 0)
+            lastRandom = UInt32(r)
             uuid += String(x == 15 ? r&0x3|0x8 : r, radix: 16)
         }
 
