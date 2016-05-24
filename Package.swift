c308a10508ae4b395c34275e59e891c7c5d5d60a
clay <clay.evil@gmail.com>
2016-05-24 19:15:00 +0200

    dependencies -> testDependencies

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "PathKit",
-    dependencies: [
+    testDependencies: [
         .Package(url: "https://github.com/EvilClay/CatchingFire.git", majorVersion: 0, minor: 2),
     ]
 )
